ef7c42fcfffcbc7c9b611c8b83734dbeec3319c6
ork23 <p.anderson2390@gmail.com>
2018-08-13 11:31:02 -0400

    Code: conection to api of openweathermap whit fetch

--- Listen.js
+++ Listen.js
@@ -1,14 +1,17 @@
 const mysql = require('mysql');
 const objtime = require('timers');
-
+var fetch = require("node-fetch");
+let cTemp='000';
 let con = mysql.createConnection({
     host: "127.0.0.1",
     user: "prueba",
     password: "123456",
     database:"bd_HistoricWeather"
-});   
-function apiUrlWeather(){
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID="+clientIdWeather;
+});  
+const clientIdWeather = "65b4b079ea9713f18c751ab13470af86";
+ 
+function apiUrlWeather(lon, lat){
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?"+lat+"&"+lon+"&units=metric&APPID="+clientIdWeather;
     console.log(apiUrl);
     return apiUrl.toString();
    
@@ -37,18 +40,20 @@ let ubication = {
 }
        
           let dataWeather=[];
-        const timeoutObj = setInterval(getAllWeatherFomObject(ubication), 1500);
+        const timeoutObj = setInterval(getAllWeatherFomObject(ubication), 60000);
        
         function getAllWeatherFomObject (obj){
             var lim= Object.keys(obj).length;
             console.log(obj['0']);
             for(let i=0;i<lim;i++){
-               getWeatherForHistoric(obj[i.toString()].latitude,obj[i.toString()].longtitude);
-               console.log('entro');
+                console.log('entro ' + obj[i.toString()].longitude);
+               let aux=getWeatherForHistoric(obj[i.toString()].latitude, obj[i.toString()].longitude);
+               console.log('entro ' + aux);
+               console.log(cTemp);
             }
            
         }
-        function getWeatherForHistoric(latitude,longtitude) {
+        /*function getWeatherForHistoric(latitude,longtitude) {
             console.log("ejecuta el api clima")
             $.ajax({
               url: 'http://api.openweathermap.org/data/2.5/weather',
@@ -65,7 +70,68 @@ let ubication = {
               }
               
             })
+          }*/
+          
+          function getWeatherForHistoric(latitude,longtitude) {
+            let urlWeather =  apiUrlWeather(latitude,longtitude);
+            let result='';
+            console.log('fetch ' + urlWeather);
+            fetch(urlWeather)
+            .then(  
+                function(response) {  
+                  if (response.status !== 200) {  
+                    console.log('Looks like there was a problem. Status Code: ' +  
+                      response.status);  
+                    return;  
+                  }
+                  // Examine the text in the response  
+                  response.json().then(function(data) {  		        
+                    // Parse data
+                    description = data["weather"][0]["description"];
+                    currenttemp = data["main"]["temp"];
+                    mintemp = data["main"]["temp_min"];
+                    maxtemp = data["main"]["temp_max"];
+                    cTemp=currenttemp;
+                });  
+		    }  
+		  )  
+                .catch(function(error) {
+                // If there is any error you will catch them here
+                console.error(error);
+                });   
+                console.log(result);
           }
+          /*fetch(api)  
+		  .then(  
+		    function(response) {  
+		      if (response.status !== 200) {  
+		        console.log('Looks like there was a problem. Status Code: ' +  
+		          response.status);  
+		        return;  
+		      }
+		      // Examine the text in the response  
+		      response.json().then(function(data) {  		        
+		        // Parse data
+		        description = data["weather"][0]["description"];
+		        currenttemp = data["main"]["temp"];
+		        mintemp = data["main"]["temp_min"];
+		        maxtemp = data["main"]["temp_max"]
+
+		        sendHTML = "<ul>\n" + 
+		        			"<li>Description: " + description + "</li>\n" + 
+		        			"<li>Current temperature: " + currenttemp + "&deg;C</li>\n" + 
+		        			"<li>Minimum temperature: " + mintemp + "&deg;C</li>\n" + 
+		        			"<li>Maximum temperature: " + maxtemp + "&deg;C</li>\n" + 
+		        			"</ul>";
+
+		        weather.insertAdjacentHTML('afterbegin', sendHTML);
+
+		      });  
+		    }  
+		  )  
+		  .catch(function(err) {  
+		    console.log('Fetch Error :-S', err); 
+		  });*/
           function guardarObjetoEnMysql( conexion, objWeather){
             let sql = "INSERT INTO weather VALUES (0,"+objWeather.temperature+","+objWeather.date+","+objWeather.idUbication+")";
             console.log(sql);
