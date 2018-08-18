60e9623b1605f245f3f079ca0ac2fe14d691c16e
paul <p.anderson2390@gmail.com>
2018-08-17 23:24:53 -0400

    feature: added function create,read,update,delete

--- Listen.js
+++ Listen.js
@@ -1,5 +1,16 @@
 const mysql = require('mysql');
 const fetch = require("node-fetch");
+var express = require('express');
+var bodyParser = require('body-parser'); 
+var mysql = require('mysql');
+var objExpress = express(); 
+objExpress.use(bodyParser.json());
+objExpress.use(bodyParser.urlencoded({ extended: true })); 
+objExpress.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 let con = mysql.createConnection({
     host: "127.0.0.1",
@@ -95,3 +106,138 @@ let ubication = {
             });
         
         }
+
+
+    objExpress.get('/datos', function(req, res) {
+    var query = "SELECT * FROM datos";
+    con.query(query, function(err, rows, col) {
+    if(err) {
+        res.write(JSON.stringify({
+        error: true,
+        error_object: err         
+       }));
+       res.end();
+     } else {
+       res.write(JSON.stringify(rows));
+       res.end();       
+     }
+   });
+});
+ 
+function CreateDatos(objDato,resp) {  
+  console.log(objDato);
+  var query = "INSERT INTO datos VALUES (";
+  query += "'" + objDato.nombre + "','" +objDato.descriptionWeather+"','"+objDato.currentTemp+"','"+objDato.dateWeather+"','"+objDato.longitude+"','"+objDato.latitude+"','"+objDato.tempMin+"','"+objDato.tempoMax+"')";   
+  con.query(query, function(err, rows, col) {
+    if(err) {
+     resp.write(JSON.stringify({
+        error: true,
+        error_object: err
+      }));
+     resp.end();
+    } else {
+      var iIDCreated = rows.insertId;
+     resp.write(JSON.stringify({
+        error: false,
+        idCreated: iIDCreated
+      }));
+     resp.end();      
+    }    
+  });
+} 
+ 
+function ReadDatos(resp) {
+  var query = "SELECT * FROM datos";
+  con.query(query, function(err, rows, col) {
+    if(err) {
+     resp.write(JSON.stringify({
+        error: true,
+        error_object: err
+      }));
+     resp.end();
+    } else {
+     resp.write(JSON.stringify({
+        error: false,
+        data: rows
+      }));
+     resp.end();            
+    }    
+  });    
+}
+function UpdateDatos(objDato,resp) {
+  var query = "UPDATE datos SET last_updated = NOW() ";
+  if(objDato.hasOwnProperty('nombre')) {
+    query += " AND nombre = '" + objDato.nombre + "' ";
+  } 
+  query = " WHERE id = '" + objDato.id + "'";
+  
+  con.query(query, function(oErrUpdate, rowsUpdate, colUpdate) {
+    if(oErrUpdate) {
+     resp.write(JSON.stringify({ 
+        error: true,
+        error_object: oErrUpdate
+      }));
+     resp.end();      
+    } else {
+     resp.write(JSON.stringify({
+        error: false
+      }));
+     resp.end();
+    }
+  });
+}
+function DeleteDato(objDato,resp) {
+  var query = "DELETE FROM datos WHERE id = '" + objDato.id + "'";
+  con.query(query, function(oErrDelete, rowsDelete, colDelete) {
+    if(oErrDelete) {
+     resp.write(JSON.stringify({
+        error: true,
+        error_object: oErrDelete
+      }));
+     resp.end();
+    } else {
+     resp.write(JSON.stringify({
+        error: false
+      }));
+     resp.end();      
+    }    
+  });  
+}
+ 
+ objExpress.post('/datos', function(req, res) {
+   var oDataOP = {};
+   var sOP = '';
+   
+   oDataOP = req.body.data_op;
+   sOP = req.body.op;
+   
+   switch(sOP) {
+     
+     case 'CREATE':      
+      CreateDatos(oDataOP, res);
+     break;
+     
+     case 'READ':
+      ReadDatos(res);
+     break;
+     
+     case 'UPDATE':
+      UpdateDatos(oDataOP, res);
+     break;
+     
+     case 'DELETE':
+      DeleteDato(oDataOP, res);
+     break;
+     
+     default:
+      res.write(JSON.stringify({ 
+        error: true, 
+        error_message: 'there has not been an operation ' 
+      }));
+      res.end();
+     break;
+     
+   }   
+ });
+ 
+ 
